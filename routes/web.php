13a78f65b97fff0969c1914095ea07bcda1681f4
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-11-06 21:34:53 +0600

    admin penal building

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,13 @@ use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/admin/users', function () {
+    return view('admin.users.index');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
