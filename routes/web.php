819442790d10dec28131cae0e79143c73b17f5ed
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-11-06 14:24:58 +0600

    Added multi-role login system

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\User\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Admin ////////////////////////////////////////////////////////////////////////
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin','verified']], function () {
+    Route::get('dashboard', [AdminController::class,'index'])->name('dashboard');
+   
+});
+// User ////////////////////////////////////////////////////////////////////////
+Route::group(['prefix' => 'user', 'as' => 'user.', 'namespace' => 'User', 'middleware' => ['auth', 'user', 'verified']], function () {
+    Route::get('dashboard', [UserController::class,'index'])->name('dashboard');
+   
+});
+
