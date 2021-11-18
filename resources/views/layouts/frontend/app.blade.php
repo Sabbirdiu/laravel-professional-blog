2c4977431c11460b0ae23acd7eb53f9d42ceb41f
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-11-18 10:10:39 +0600

    Added frontend setup

---
+++ resources/views/layouts/frontend/app.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Bootstrap Blog - B4 Template by Bootstrap Temple</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="{{asset('frontend/css/linearicons.css')}}" />
+    <link rel="stylesheet" href="{{asset('frontend/vendor/bootstrap/css/bootstrap.min.css')}}">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="{{asset('frontend/vendor/font-awesome/css/font-awesome.min.css')}}">
+    <!-- Custom icon font-->
+    <link rel="stylesheet" href="{{asset('frontend/css/fontastic.css')}}">
+    <!-- Google fonts - Open Sans-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <!-- Fancybox-->
+    <link rel="stylesheet" href="vendor/@fancyapps/fancybox/jquery.fancybox.min.css">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="{{asset('frontend/css/style.default.css')}}" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="{{asset('frontend/css/custom.css')}}">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="favicon.png">
+   
+  </head>
+  <body>
+  @include('layouts.frontend.partials.navbar')
+ 
+  @yield('content')
+  @include('layouts.frontend.partials.footer')
+ 
+    <!-- JavaScript files-->
+    <script src="{{asset('frontend/js/vendor/bootstrap.min.js')}}"></script>
+    <script src="{{asset('frontend/vendor/jquery/jquery.min.js')}}"></script>
+   
+   
+    <script src="{{asset('frontend/js/front.js')}}"></script>
+    <script src="{{asset('frontend/vendor/@fancyapps/fancybox/jquery.fancybox.min.js')}}"></script>
+    <script src="{{asset('frontend/vendor/popper.js/umd/popper.min.js')}}"></script>
+    <script src="{{asset('frontend/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('frontend/vendor/jquery.cookie/jquery.cookie.js')}}"></script>
+
+    
+  
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
-Route::get('/home', [HomeController::class, 'index'])->name('home');
 
+Auth::routes();
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 
